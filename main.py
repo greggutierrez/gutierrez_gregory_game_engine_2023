a5de05b593f3861d6ff6d9ee78904edab3f7d36d
greggutierrez <142935167+greggutierrez@users.noreply.github.com>
2023-10-20 13:37:27 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 import pygame as pg
 from pygame.sprite import Sprite
 import random
+from random import randint
 import os
+from settings import *
 
 vec = pg.math.Vector2
 
@@ -13,23 +15,7 @@ game_folder = os.path.dirname(__file__)
 img_folder = os.path.join(game_folder, 'images')
 snd_folder = os.path.join(game_folder, 'sounds')
 
-# game settings 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-SCORE = 0
 
-# player settings
-PLAYER_JUMP = 30
-PLAYER_GRAV = 1.5
-
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-SKYBLUE = (150, 200, 255)
 
 def draw_text(text, size, color, x, y):
     font_name = pg.font.match_font('arial')
@@ -40,7 +26,9 @@ def draw_text(text, size, color, x, y):
     screen.blit(text_surface, text_rect)
 
 class Player(Sprite):
+    # this is the init method where you can setup properties for a class
     def __init__(self):
+        # call the super class init method
         Sprite.__init__(self)
         # self.image = pg.Surface((50, 50))
         # self.image.fill(GREEN)
@@ -51,6 +39,7 @@ class Player(Sprite):
         self.pos = vec(WIDTH/2, HEIGHT/2)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
+        self.cofric = -0.3
     def controls(self):
         keys = pg.key.get_pressed()
         if keys[pg.K_a]:
@@ -68,13 +57,18 @@ class Player(Sprite):
         self.acc = vec(0,PLAYER_GRAV)
         self.controls()
         # friction for side to side 
-        self.acc.x += self.vel.x * -0.6
+        self.acc.x += self.vel.x * self.cofric
 
         # equations of motion
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
 
         self.rect.midbottom = self.pos
+        # check to see if he fell off the bottom
+        if self.rect.y > HEIGHT:
+            self.pos = vec(WIDTH/2, HEIGHT/2)
+            
+
 
 # platforms
 
@@ -82,7 +76,7 @@ class Platform(Sprite):
     def __init__(self, x, y, w, h, category):
         Sprite.__init__(self)
         self.image = pg.Surface((w, h))
-        self.image.fill(WHITE)
+        self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -94,6 +88,35 @@ class Platform(Sprite):
             self.rect.x += self.speed
             if self.rect.x + self.rect.w > WIDTH or self.rect.x < 0:
                 self.speed = -self.speed
+        if self.category == "ice":
+            self.image.fill(WHITE)
+
+class Mob(Sprite):
+    def __init__(self, x, y, w, h, category):
+        Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        print(self.rect.center)
+        self.category = category
+        self.speed = 10
+    def update(self):
+        if self.category == "moving":
+            self.rect.x += self.speed
+            if self.rect.x + self.rect.w > WIDTH or self.rect.x < 0:
+                self.speed = -self.speed
+                self.rect.y += 25
+        if self.category == "ice":
+            self.image.fill(WHITE)
+
+        if self.rect.y > HEIGHT:
+            self.rect.y = 0
+            # self.kill()
+            # print("this happened")
+            print(all_mobs)
+     
 
 
 
@@ -107,11 +130,16 @@ clock = pg.time.Clock()
 # create a group for all sprites
 all_sprites = pg.sprite.Group()
 all_platforms = pg.sprite.Group()
+all_mobs = pg.sprite.Group()
 
 # instantiate classes
 player = Player()
-plat = Platform(150, 300, 100, 30, "  ")
+plat = Platform(150, 300, 100, 30, "ice")
 plat1 = Platform(200, 200, 100, 30, "moving")
+
+
+
+
 
 # add instances to groups
 all_sprites.add(player)
@@ -120,28 +148,46 @@ all_sprites.add(plat1)
 all_platforms.add(plat)
 all_platforms.add(plat1)
 
+for i in range(0,20):
+    m = Mob(randint(0,WIDTH),randint(0,HEIGHT),25,25,"moving")
+    all_sprites.add(m)
+    all_mobs.add(m)
+
+for plat in PLATFORM_LIST:
+    p = Platform(*plat)
+    all_sprites.add(p)
+    all_platforms.add(p)
 
 # Game loop
 running = True
+
+
+
 while running:
     # keep the loop running using clock
-    clock.tick(FPS)
+    currentfps = clock.tick(FPS)
         
     for event in pg.event.get():
         # check for closed window
         if event.type == pg.QUIT:
             running = False
     
-    ############ Update ##############
+    ############ Game Update Loop Section ##############
     # update all sprites
+    # (the player controls or input happen in player update method)
     all_sprites.update()
-
+    
     # this is what prevents the player from falling through the platform when falling down...
     if player.vel.y > 0:
             hits = pg.sprite.spritecollide(player, all_platforms, False)
             if hits:
+                if hits[0].category == "moving":
+                    player.vel.x = hits[0].speed*1.75
+
                 player.pos.y = hits[0].rect.top
                 player.vel.y = 0
+
+                
                 
     # this prevents the player from jumping up through a platform
     if player.vel.y < 0:
@@ -154,12 +200,16 @@ while running:
                 player.acc.y = 5
                 player.vel.y = 0
 
+    mhits = pg.sprite.spritecollide(player, all_mobs, True)
+    if mhits:
+        mhits[0].kill()
+        print(all_mobs)
     ############ Draw ################
     # draw the background screen
-    screen.fill(SKYBLUE)
     # draw all sprites
+    screen.fill(SKYBLUE)
     all_sprites.draw(screen)
-    draw_text("Score: " + str(SCORE), 22, WHITE, WIDTH/2, HEIGHT/10)
+    draw_text("FPS: " + str(currentfps), 22, BLACK, WIDTH/2, HEIGHT/10)
 
     # buffer - after drawing everything, flip display
     pg.display.flip()
